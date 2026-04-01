cc0fbe0efe69bca39ec948cd3ebce77db87b8828
kobayashi-ryotaro-git <kobayashi.ryotaro@t.dentsusoken.com>
2026-04-01 17:11:46 +0900

    Create main.tf for RDS module

---
+++ modules/rds/main.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "this" {
+  allocated_storage    = var.allocated_storage
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = var.instance_class
+  name                 = var.db_name
+  username             = var.username
+  password             = var.password
+  parameter_group_name = var.parameter_group_name
+  skip_final_snapshot  = var.skip_final_snapshot
+}
